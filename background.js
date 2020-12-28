19cf3a8a58c56ea3318d8b401df2b1e740b30208
Doug Harris <dharris@gmail.com>
2020-12-28 16:15:48 -0500

    Cleaner code & saves/loads preferred versions
    - Added reading and writing from storage
    - Modularized the javascript
    - Added test functions to set & view stored versions

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-let docsData = {
+const docsData = {
     django: {
         version: "2.2",
         urls: "https://docs.djangoproject.com/*",
@@ -12,44 +12,103 @@ let docsData = {
 };
 
 
-function listener(details) {
-    let thisVersionUrl = details.url;
-    let redirectUrl = '';
-    if (thisVersionUrl.includes("django")) {
-        let myInfo = docsData.django;
+
+let redirector = (function() {
+    let preferredVersions = {};
+
+    let setDefaultVersions = function() {
+        console.log("Setting default versions");
+        preferredVersions = {
+            "django": "1.11",
+            "python": "3.8"
+        };
+        browser.storage.local.set({'preferredVersions': preferredVersions});
+    };
+
+    let redirectDocs = function(originUrl, platform) {
+        console.log(`Redirecting for ${platform}.`);
+        let versionKey = `${platform}_version`;
+        let myVersion = preferredVersions[platform];
+        let myInfo = docsData[platform];
         let regexp = new RegExp(myInfo.pattern);
-        let match = regexp.exec(thisVersionUrl);
+        let match = regexp.exec(originUrl);
         let thisVersion = match.groups.version;
-        console.log(`Matched django version ${match.groups.version} `);
-        if (thisVersion != myInfo.version) {
-            redirectUrl = thisVersionUrl.replace(thisVersion, myInfo.version);
-            console.log(`Redirecting to version ${myInfo.version} to ${redirectUrl}`);
+        let redirectUrl = '';
+        if (!myVersion) {
+            console.log(`Couldn't find ${platform} version`);
         }
-    } else if (thisVersionUrl.includes("python")) {
-        let myInfo = docsData.python;
-        let regexp = new RegExp(myInfo.pattern);
-        let match = regexp.exec(thisVersionUrl);
-        // let thisVersion = 
-        console.log(`Matched python version ${match.groups.version} `);
-    }
-    if (redirectUrl) {
-        return { redirectUrl: redirectUrl };
-    } else {
-        return {};
-    }
-}
-
-
-var urlPatterns = [];
-
-for (var key in docsData) {
-    urlPatterns.push(docsData[key].urls);
-    console.log(`A: URL patterns ${urlPatterns}`);
-}
-console.log(`URL patterns ${urlPatterns}`);
+        console.log(`Matched ${platform} version ${match.groups.version} `);
+        if (thisVersion != myVersion) {
+            redirectUrl = originUrl.replace(thisVersion, myVersion);
+            console.log(`Redirecting to version ${myVersion} to ${redirectUrl}`);
+        }
+        return redirectUrl;
+    };
     
+
+    return {
+        listener: function(details) {
+            let originUrl = details.url;
+            let redirectUrl = '';
+            if (originUrl.includes("django")) {
+                redirectUrl = redirectDocs(originUrl, "django");
+            } else if (thisVersionUrl.includes("python")) {
+                let myInfo = docsData.python;
+                let regexp = new RegExp(myInfo.pattern);
+                let match = regexp.exec(thisVersionUrl);
+                console.log(`Matched python version ${match.groups.version} `);
+            }
+            if (redirectUrl) {
+                console.log(`listener redirecting.`);
+                return { redirectUrl: redirectUrl };
+            } else {
+                return {};
+            }
+        },
+        loadPreferredVersions: function() {
+            console.log("Loading preferred versions");
+            browser.storage.local.get('preferredVersions').then(
+                function(items) {
+                    if (Object.keys(items).length == 0) {
+                        setDefaultVersions();
+                    }
+                    for (let p in items) {
+                        preferredVersions = items.preferredVersions;
+                    }
+                    
+                });
+        },
+        showStoredVersions: function() {
+            let output = '';
+            for (let p in preferredVersions) {
+                output = output + `\t${p}: ${preferredVersions[p]}\n`;
+            }
+            console.log(`Preferred versions:\n${output}`);
+        },
+        setTestVersion: function(platform, version) {
+            console.log(`Setting ${platform} to ${version}`);
+            preferredVersions[platform] = version;
+            browser.storage.local.set({'preferredVersions': preferredVersions});
+        }
+
+    };
+})();
+
+redirector.loadPreferredVersions();
+
+
+// DEBUG patterns used
+let urlPatterns = [];
+
+for (let key in docsData) {
+    urlPatterns.push(docsData[key].urls);
+}
+console.log(`Will redirect for: ${urlPatterns}`);
+
+   
 browser.webRequest.onBeforeRequest.addListener(
-    listener,
+    redirector.listener,
     { urls: urlPatterns, types: ["main_frame"] },
     ["blocking"]
 );
+
